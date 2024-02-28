2ade239ab05ccaec593cb551f720cb0ae0b007d0
Steven Silvester <steve.silvester@mongodb.com>
2024-02-28 05:18:06 -0600

    fix path handling

--- scripts/check_anchors/check-anchors.sh
+++ scripts/check_anchors/check-anchors.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 SCRIPT_DIR="$( cd -- "$SCRIPT_DIR" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR
 npm install 
-node index.js "$@"
+popd 
+node $SCRIPT_DIR/index.js "$@"
